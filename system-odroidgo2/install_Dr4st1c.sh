f9e1dd6ad411f6018a5843c217a1e8202c1eba90
npaladin2000 <43549566+npaladin2000@users.noreply.github.com>
2020-05-27 20:03:55 -0400

    Create install_Dr4st1c.sh

---
+++ system-odroidgo2/install_Dr4st1c.sh
@@ -0,0 +1,9 @@
+##/bin/bash
+
+LINK="https://raw.githubusercontent.com/shantigilbert/binaries/master/odroid-xu4/drastic.tar.gz"
+LINKDEST="~/drastic.tar.gz"
+wget -O $LINKDEST $LINK
+tar xvf $LINKDEST 
+rm $LINKDEST
+mkdir ~/drastic/config
+cp ~/.ds_configs/drastic.* /~/drastic/config/
