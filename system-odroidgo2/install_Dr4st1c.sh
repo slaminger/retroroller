9ea0e25357432bcffe8c8c52ae248be8cc348784
npaladin2000 <43549566+npaladin2000@users.noreply.github.com>
2020-05-28 08:17:12 -0400

    Delete install_Dr4st1c.sh
    Moved to /drastic folder

--- system-odroidgo2/install_Dr4st1c.sh
+++
@@ -1,8 +0,0 @@
-##/bin/bash
-
-LINK="https://raw.githubusercontent.com/shantigilbert/binaries/master/odroid-xu4/drastic.tar.gz"
-LINKDEST="~/drastic.tar.gz"
-wget -O $LINKDEST $LINK
-tar xvf $LINKDEST 
-rm $LINKDEST
-
